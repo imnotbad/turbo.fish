ec0c463b8ded8635df8c5765e697c4c29e31b2ac
Kaidan <fishstacks@protonmail.com>
2017-11-13 10:45:43 +1100

    Variable tail lengths work, still some bugs

--- src/turbofish.rs
+++ src/turbofish.rs
@@ -10,13 +10,23 @@ impl TurboFish {
 }
 
 impl<'a> FromParam<'a> for TurboFish {
+    //TODO: fix bug that causes extra <> to appear
     type Error = &'a RawStr;
 
     fn from_param(param: &'a RawStr) -> Result<Self, Self::Error> {
         let param_cow = param.percent_decode().map_err(|_| param)?;
-        
-        if param_cow.starts_with(':') && param_cow.contains(":<") && param_cow.ends_with('>') {
-            Ok(TurboFish(param_cow.replacen("<", "<​", 1)))
+        let mut param_content = String::new();
+        if param_cow.starts_with(':') && param_cow.ends_with('>') {
+            for (i, letter) in param_cow.char_indices() {
+                if letter == ':' && param_cow.chars().nth(i+1) == Some('<') {
+                    param_content = param_cow.split_at(i+1).1.to_string();
+                    break
+                
+                } else if letter != ':' {
+                    return Err(param);
+                }
+            }
+            Ok(TurboFish(param_content.replace("<", "<​")))
         } else {
             Err(param)
         }
