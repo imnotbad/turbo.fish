9129988726fdbb1578757395d628f6f38cf6eb1b
Kaidan <fishstacks@protonmail.com>
2017-11-21 20:23:01 +1100

    Fixed bugs

--- src/turbofish.rs
+++ src/turbofish.rs
@@ -10,25 +10,31 @@ impl TurboFish {
 }
 
 impl<'a> FromParam<'a> for TurboFish {
-    //TODO: fix bug that causes extra <> to appear
     type Error = &'a RawStr;
 
     fn from_param(param: &'a RawStr) -> Result<Self, Self::Error> {
         let param_cow = param.percent_decode().map_err(|_| param)?;
-        let mut param_content = String::new();
+        let mut end_tail: usize = 0;
+        let mut is_valid = false;
         if param_cow.starts_with(':') && param_cow.ends_with('>') {
             for (i, letter) in param_cow.char_indices() {
-                if letter == ':' && param_cow.chars().nth(i+1) == Ok('<') {
-                    param_content = param_cow.split_at(i+1).1.to_string();
-                    break
-                
-                } else if letter != ':' {
-                    return Err(param);
+                if letter == '<' {
+                    // Ugly flag for valid URL because of my code structure here
+                    is_valid = true;
+                    // Check if every character *before* the < is a colon
+                    if param_cow[..i].chars().all(|character| character == ':') {
+                        end_tail = i+1;
+                        break;
+                    } else {
+                        return Err(param);
+                    }
                 }
             }
-            Ok(TurboFish(param_content.replace("<", "<​")))
-        } else {
-            Err(param)
+            if is_valid {
+                // modify any <> within user input to contain a zero-width-space to prevent breakage
+                return Ok(TurboFish(param_cow[end_tail..param_cow.len()-1].replace("<", "<​")));
+            }
         }
+        Err(param)
     }
 }
